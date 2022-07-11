4174c8a09f683c059686ce4074d3a1ad2b3086c5
EarlyAnt <54763755@qq.com>
2022-07-11 15:43:48 +0800

    1.第2页刷新进度前的等待时间，增加3个状态显示。 2.第1页下方的版本日期改为版本号。

--- muse/src/js/app/scenes/scene1.js
+++ muse/src/js/app/scenes/scene1.js
@@ -4,11 +4,17 @@ this.scene1 = function (taskId, prompt_tanslation, newTask) {
     console.log("scene1->prompt_tanslation: " + prompt_tanslation);
     console.log("scene1->newTask: " + newTask);
 
+    let INITIALIZING = "正在初始化...";
+    let MODEL_LOADING = "正在加载模型...";
+    let MODEL_CALCULATING = "正在匹配参数...";
+    let waitTextStarted = false;
+
     const START_TASK = "即将开始生成图片...";
     const WAIT_TASK = "任务排队中...";
     const LAST_TASK = "正在尝试继续上一个任务...";
     const TASK_DURATION = 2;
 
+    let imageCreating = false;
     let clickTimes = 0;
     let taskIdInCookie = COOKIE.getCookie("taskId");
 
@@ -39,9 +45,36 @@ this.scene1 = function (taskId, prompt_tanslation, newTask) {
     });
 
     refresh();
-    let timer = setInterval(() => {
+    let timerTask = setInterval(() => {
         refresh();
     }, 10000);
+
+    let timerWait;
+    function showWaitText() {
+        if (waitTextStarted)
+            return;
+
+        waitTextStarted = true;
+        let seconds = 0;
+        timerWait = setInterval(() => {
+            if (imageCreating) {
+                clearInterval(timerWait);
+                return;
+            }
+
+            if (seconds >= 0 && seconds < 10) {
+                txtProgress.innerText = INITIALIZING;
+            } else if (seconds >= 10 && seconds < 25) {
+                txtProgress.innerText = MODEL_LOADING;
+            } else if (seconds >= 25 && seconds < 45) {
+                txtProgress.innerText = MODEL_CALCULATING;
+            } else if (seconds >= 45) {
+                txtProgress.innerText = START_TASK;
+            }
+
+            seconds += 1;
+        }, 1000);
+    }
 
     async function refresh() {
         try {
@@ -57,12 +90,13 @@ this.scene1 = function (taskId, prompt_tanslation, newTask) {
             if (response.progress == 0) {
                 var queueNumber = response.queue_num;
                 if (queueNumber == 0) {
-                    txtProgress.innerText = START_TASK;
+                    showWaitText();
                 }
                 else {
                     txtProgress.innerText = WAIT_TASK + "(大约等待" + (queueNumber * TASK_DURATION).toFixed(0) + "分钟)";
                 }
             } else if (response.progress > 0) {
+                imageCreating = true;
                 txtProgress.innerText = "正在生成图片...(" + response.progress + "%)";
                 SERVER.download(response.progress_img, (imageObj) => {
                     imgProcedure.src = imageObj;
@@ -71,7 +105,7 @@ this.scene1 = function (taskId, prompt_tanslation, newTask) {
             }
 
             if (response.progress == 100) {
-                clearTimeout(timer);
+                clearInterval(timerTask);
                 setTimeout(() => {
                     document.getElementById("divPage2").style.display = "none";
                     switchScene(2, taskId, response.progress_img);

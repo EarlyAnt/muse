06adb143f8254caa6e4c0c0e3c54d42f59c50aed
EarlyAnt <54763755@qq.com>
2022-07-20 17:17:02 +0800

    1.第3页，分享页，也改成了vue的方式。

--- muse/mvp1/src/js/app/scenes/scene2.js
+++ muse/mvp1/src/js/app/scenes/scene2.js
@@ -3,72 +3,100 @@ this.scene2 = function (taskId, imagePath) {
     console.log("scene2->taskId: " + taskId);
     console.log("scene2->imagePath: " + imagePath);
 
-
     let scene = new PIXI.Container();
     mainContainer.addChild(scene);
     // overlay.visible = true;
 
-    let clickTimes = 0;
+    var vm = new Vue({
+        el: '#divPage3',
+        data: {
+            divPage3: null,
+            txtSetting: null,
+            imgComplete: null,
 
-    document.getElementById("divPage3").style.display = "";
+            settingRepeatTimes: 0,
+            imageRepeatTimes: 0,
+            repeatTimesLimit: 10,
+            clickTimes: 0,
 
-    let txtSetting = document.getElementById("txtSetting");
-    let imgComplete = document.getElementById("imgComplete");
-
-    let settingRepeatTimes = 0;
-    let imageRepeatTimes = 0;
-    let repeatTimesLimit = 10;
-    imgComplete.onerror = () => {
-        console.log("scene2.loadImage->repeat times: " + imageRepeatTimes);
-        if (imageRepeatTimes < repeatTimesLimit) {
-            setTimeout(() => { imgComplete.src = imagePath; }, 1000);
-        }
-        imageRepeatTimes += 1;
-    };
-    imgComplete.src = imagePath;
-
-    COOKIE.setCookie("taskId", "", -1);
-    COOKIE.setCookie("translation", "", -1);
-
-    document.getElementById("btnRetry").addEventListener("click", () => {
-        location.reload();
-    });
-
-    document.getElementById("txtShare").addEventListener("click", () => {
-        clickTimes += 1;
-        if (clickTimes == 3) {
-            imgComplete.style.display = "none";
-            txtSetting.style.display = "";
-        } else if (clickTimes == 4) {
-            clickTimes = 0;
-            imgComplete.style.display = "";
-            txtSetting.style.display = "none";
-        }
-    });
-
-    async function getSetting() {
-        try {
-            var response = await SERVER.callApi(params = {
-                path: "query_settings?task_id=" + taskId,
-                Headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Method": "GET, POST",
+            baseUrl: 'http://region-4.autodl.com:40410/api/',
+        },
+        methods: {
+            goback: function () {
+                this.clickTimes += 1;
+                if (this.clickTimes == 3) {
+                    this.imgComplete.style.display = "none";
+                    this.txtSetting.style.display = "";
+                } else if (this.clickTimes == 4) {
+                    this.clickTimes = 0;
+                    this.imgComplete.style.display = "";
+                    this.txtSetting.style.display = "none";
                 }
-            });
-            console.log("----get setting response----");
-            console.log(response);
-            txtSetting.value = JSON.stringify(response, null, "\t");
-        } catch (e) {
-            console.log("scene2.getSetting->error: ");
-            console.log(e);
+            },
+            reload: function () {
+                location.reload();
+            },
+            getSetting: async function () {
+                try {
+                    this.$http.get(this.baseUrl + "query_settings?task_id=" + taskId).then(function (result) {
+                        console.log("----get setting response----");
+                        console.log(result);
+                        this.txtSetting.value = JSON.stringify(result.body, null, "\t");
+                    });
+                } catch (e) {
+                    console.log("scene2.getSetting->error: ");
+                    console.log(e);
 
-            if (settingRepeatTimes < repeatTimesLimit) {
-                console.log("scene2.getSetting->repeat times: " + settingRepeatTimes);
-                setTimeout(getSetting, 1000);
+                    if (this.settingRepeatTimes < this.repeatTimesLimit) {
+                        console.log("scene2.getSetting->repeat times: " + this.settingRepeatTimes);
+                        setTimeout(getSetting, 1000);
+                    }
+                    this.settingRepeatTimes += 1;
+                }
             }
-            settingRepeatTimes += 1;
-        }
-    }
+        },
+        beforeCreate() {
+            console.log("beforeCreate->time: " + new Date());
+        },
+        created() {
+            console.log("created->time: " + new Date());
+        },
+        beforeMount() {
+            console.log("beforeMount->time: " + new Date());
+        },
+        async mounted() {
+            console.log("mounted->time: " + new Date());
 
-    getSetting();
+            this.divPage3 = document.getElementById("divPage3");
+            this.txtSetting = document.getElementById("txtSetting");
+            this.imgComplete = document.getElementById("imgComplete");
+            this.divPage3.style.display = "";
+
+            this.imgComplete.onerror = () => {
+                console.log("scene2.loadImage->repeat times: " + this.imageRepeatTimes);
+                if (this.imageRepeatTimes < this.repeatTimesLimit) {
+                    setTimeout(() => { this.imgComplete.src = imagePath; }, 1000);
+                }
+                this.imageRepeatTimes += 1;
+            };
+            this.imgComplete.src = imagePath;
+
+            // COOKIE.setCookie("taskId", "", -1);
+            // COOKIE.setCookie("translation", "", -1);
+
+            this.getSetting();
+        },
+        beforeUpdate() {
+            console.log("beforeUpdate->time: " + new Date());
+        },
+        updated() {
+            console.log("updated->time: " + new Date());
+        },
+        beforeDestroy() {
+            console.log("beforeDestroy->time: " + new Date());
+        },
+        destroyed() {
+            console.log("destroyed->time: " + new Date());
+        },
+    });
 }

d78832c69a08e537cea98f32b544785ba764036e
EarlyAnt <54763755@qq.com>
2022-07-21 16:14:43 +0800

    1.增加loading时点点点动效的工具类和测试脚本。

---
+++ muse/mvp1/src/js/app/bot.js
@@ -0,0 +1,30 @@
+!function () {
+    BOT = {}
+
+    let content = "";
+    let timer = null;
+
+    function start(callback, interval = 500, count = 3, symbol = '.') {
+        timer = setInterval(() => {
+            if (content.length < count) {
+                content += symbol;
+            } else {
+                content = "";
+            }
+
+            // console.log("BOT.show->content: " + content + ", count: " + count + ", interval: " + interval + ", symbol: " + symbol);
+            if (callback) {
+                callback(content);
+            }
+        }, interval);
+    }
+    BOT.start = start;
+
+    function stop() {
+        if (timer != null) {
+            clearInterval(timer);
+        }
+    }
+    BOT.stop = stop;
+
+}();
